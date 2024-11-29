b4ac3f15f87eca08ac6fc3e7fd70c19a90ec6f7a
TMML <tml3200035928@outlook.com>
2024-11-29 21:41:05 +0800

    update dataset class

--- Diffusion/Train.py
+++ Diffusion/Train.py
@@ -1,11 +1,11 @@
 
 import os
 from typing import Dict
-
+from PIL import Image
 import torch
 import torch.optim as optim
 from tqdm import tqdm
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Dataset
 from torchvision import transforms
 from torchvision.datasets import CIFAR10
 from torchvision.utils import save_image
@@ -14,19 +14,66 @@ from Diffusion import GaussianDiffusionSampler, GaussianDiffusionTrainer
 from Diffusion.Model import UNet
 from Scheduler import GradualWarmupScheduler
 
+# ?----------------------------------------------------------------
+
+transforms=transforms.Compose([
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+        ])
+
+
+class DriveDataset(Dataset):
+    def __init__(self, root: str, train: bool, transforms=None):
+        super(DriveDataset, self).__init__()
+        data_root = os.path.join(root, "aug" if train else "test")
+        assert os.path.exists(data_root), f"path '{data_root}' does not exists."
+        self.transforms = transforms
+        img_names = [i for i in os.listdir(os.path.join(data_root, "images"))]
+        self.img_list = [os.path.join(data_root, "images", i) for i in img_names]
+        # 换掉名字
+        img_names = [name.replace("training.tif", "manual1.gif") for name in img_names]
+        img_names = [name.replace("test.tif", "manual1.gif") for name in img_names]
+        self.manual = [os.path.join(data_root, "labels", i) for i in img_names]
+        # check files
+        for i in self.manual:
+            if os.path.exists(i) is False:
+                print(f"file {i} does not exists.")
+
+    # 根据idx提取图像
+    def __getitem__(self, idx):
+        img = Image.open(self.img_list[idx])
+        # 值为0和255
+        mask = Image.open(self.manual[idx]).convert('L')
+        if self.transforms is not None:
+            img = self.transforms(img)
+            mask = self.transforms(mask)
+        return img, mask
+
+    def __len__(self):
+        return len(self.img_list)
+
+
+# ?----------------------------------------------------------------
+
 
 def train(modelConfig: Dict):
     device = torch.device(modelConfig["device"])
-    # dataset
-    dataset = CIFAR10(
+
+    dataset = DriveDataset(modelConfig["root"], True, transforms)
+    
+    # !dataset-----------------------------------------------------------
+    '''dataset = CIFAR10(
         root='./CIFAR10', train=True, download=True,
         transform=transforms.Compose([
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-        ]))
+        ]))'''
+    # !dataset-----------------------------------------------------------
     dataloader = DataLoader(
         dataset, batch_size=modelConfig["batch_size"], shuffle=True, num_workers=4, drop_last=True, pin_memory=True)
+
 
     # model setup
     net_model = UNet(T=modelConfig["T"], ch=modelConfig["channel"], ch_mult=modelConfig["channel_mult"], attn=modelConfig["attn"],
